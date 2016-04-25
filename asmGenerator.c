dfa9de94c1ec65e94253e95c94090a136bc37ac7
Pongpayak Handsome <donus33@gmail.com>
2016-04-25 14:25:06 +0700

    edit alloc to asmGen, adjust assambly to be amd64

---
+++ asmGenerator.c
@@ -0,0 +1,71 @@
+#include "asmGenerator.h"
+
+char* init_var(char* taroffset){
+	char* asscode = (char *)malloc(sizeof(char*));
+	sprintf(asscode,"\txor %rax, %rax\n"); //clear reg
+	sprintf(asscode,"\tmov %rax,-%s(%rbp)\n",taroffset); //alloc var count offset from base pointer
+	return asscode;
+}
+
+char* assign(char* opOffset,char* taroffset){
+	char* asscode = (char *)malloc(sizeof(char*));
+	//if op == '' means $a = add, sub, mul, div, mod, const
+	if(op == ''){
+		sprintf(asscode,"\tpop %rax\n");
+		sprintf(asscode,"\tmov %rax,-%s(%rbp)\n",taroffset);
+	}
+	//if go else means $a = var (op is var address )
+	else{
+		sprintf(asscode,"\tmov -%s(%rbp),%s(%rbp)\n",opOffset,taroffset);
+	}
+	return asscode;
+}
+
+char* constn(char* val){
+	char* asscode = (char *)malloc(sizeof(char*));
+	sprintf(asscode, "\tmov %s,%rax\n",val); //copy cont to rax
+	sprintf(asscode, "\tpush %rax\n"); //push rax into stack
+	return asscode;
+}
+
+char* add(char* operandleft,char* operandright){
+	char* asscode = (char*)malloc(sizeof(char*));
+	//no operand passed means get it from stack
+	if(operandright == ''){
+		sprintf(asscode,"\tpop %rbx\n");
+	}
+	else{
+		sprintf(asscode,"\tmov -%s(%rbp), %rbx\n");
+	}
+	if(operandleft == ''){
+		sprintf(asscode,"\tpop %rax\n");
+	}
+	else{
+		sprintf(asscode,"\tmov -%s(%rbp), %rax\n")
+	}
+
+	sprintf(asscode,"\tadd %rbx, %rax\n");
+	sprintf(asscode,"\tpush %rax\n");
+	return asscode;
+}
+
+char* sub(char* operandleft,char* operandright){
+	char* asscode = (char*)malloc(sizeof(char*));
+	//no operand passed means get it from stack
+	if(operandright == ''){
+		sprintf(asscode,"\tpop %rbx\n");
+	}
+	else{
+		sprintf(asscode,"\tmov -%s(%rbp), %rbx\n");
+	}
+	if(operandleft == ''){
+		sprintf(asscode,"\tpop %rax\n");
+	}
+	else{
+		sprintf(asscode,"\tmov -%s(%rbp), %rax\n")
+	}
+
+	sprintf(asscode,"\tsub %rbx, %rax\n");
+	sprintf(asscode,"\tpush %rax\n");
+	return asscode;
+}
---
+++ asmGenerator.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+
+char* init_var(char* taroffset);
+char* assign(char* opOffset,char* taroffset);
+char* constn(char* val);
+char* add(char* operandleft,char* operandright);
+char* sub(char* operandleft,char* operandright);
+
